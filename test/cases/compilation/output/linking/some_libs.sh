a584501e023356eea93344e54edd41337bf6c5f1
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-05 19:53:43 +0300

    Fix tests

--- test/cases/compilation/output/linking/some_libs.sh
+++ test/cases/compilation/output/linking/some_libs.sh
@@ -2,19 +2,18 @@
 
 # REQUIRES: shell
 
-# RUN: rm -rf %T/lib* %T/other
+# RUN: ar qc %T/libsome_libs.a
+# RUN: touch %T/libsome_libs.%{dynamic_lib_extension}
 
-# RUN: ar qc %T/libmy.a
 # RUN: mkdir %T/other
-# RUN: ar qc %T/other/libmy.a
-# RUN: touch %T/libmy.%{dynamic_lib_extension}
+# RUN: ar qc %T/other/libsome_libs.a
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
 # RUN: assert_compilation %t.json count -eq 1
-# RUN: assert_compilation %t.json contains -file %T/some_libs.c -files %T/libmy.%{dynamic_lib_extension} -directory %T -arguments %{c_compiler} -c -L. -L ./other/ -lmy -o some_libs.c.o some_libs.c
+# RUN: assert_compilation %t.json contains -file %T/some_libs.c -files %T/libsome_libs.%{dynamic_lib_extension} -directory %T -arguments %{c_compiler} -c -L. -L ./other/ -lsome_libs -o some_libs.c.o some_libs.c
 # RUN: assert_compilation %t_link.json count -eq 1
-# RUN: assert_compilation %t_link.json contains -files %T/libmy.%{dynamic_lib_extension} %T/some_libs.c.o -directory %T -arguments %{c_compiler} -L. -L ./other/ -lmy some_libs.c.o -o some_libs
+# RUN: assert_compilation %t_link.json contains -files %T/libsome_libs.%{dynamic_lib_extension} %T/some_libs.c.o -directory %T -arguments %{c_compiler} -L. -L ./other/ -lsome_libs some_libs.c.o -o some_libs
 
 echo "int main() { return 0; }" > some_libs.c
 
-$CC -o some_libs -L. -L ./other/ -lmy some_libs.c
+$CC -o some_libs -L. -L ./other/ -lsome_libs some_libs.c
