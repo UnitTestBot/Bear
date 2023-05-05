529618a1c25168da4c4ee5fabd6a30339b29e0ef
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-05 19:01:20 +0300

    Fix test

--- test/cases/compilation/output/linking/some_dir_for_libs.sh
+++ test/cases/compilation/output/linking/some_dir_for_libs.sh
@@ -4,16 +4,16 @@
 
 # RUN: rm -rf %T/lib* %T/other
 
-# RUN: ar qc %T/libmy.a
+# RUN: ar qc %T/liba.a
 # RUN: mkdir %T/other
-# RUN: ar qc %T/other/libmy.a
+# RUN: ar qc %T/other/liba.a
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
 # RUN: assert_compilation %t.json count -eq 1
-# RUN: assert_compilation %t.json contains -file %T/some_dir_for_libs.c -files %T/other/libmy.a -directory %T -arguments %{c_compiler} -c -L ./other/ -L. -lmy -o some_dir_for_libs.c.o some_dir_for_libs.c
+# RUN: assert_compilation %t.json contains -file %T/some_dir_for_libs.c -files %T/other/liba.a -directory %T -arguments %{c_compiler} -c -L ./other/ -L. -la -o some_dir_for_libs.c.o some_dir_for_libs.c
 # RUN: assert_compilation %t_link.json count -eq 1
-# RUN: assert_compilation %t_link.json contains -files %T/other/libmy.a %T/some_dir_for_libs.c.o -directory %T -arguments %{c_compiler} -L ./other/ -L. -lmy some_dir_for_libs.c.o -o some_dir_for_libs
+# RUN: assert_compilation %t_link.json contains -files %T/other/liba.a %T/some_dir_for_libs.c.o -directory %T -arguments %{c_compiler} -L ./other/ -L. -la some_dir_for_libs.c.o -o some_dir_for_libs
 
 echo "int main() { return 0; }" > some_dir_for_libs.c
 
-$CC -o some_dir_for_libs -L ./other/ -L. -lmy some_dir_for_libs.c
+$CC -o some_dir_for_libs -L ./other/ -L. -la some_dir_for_libs.c
