1ae5076e7082e5bfbf3bb0be16ff901be79b6145
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-16 15:41:28 +0300

    Fix tests for mac-os

--- test/cases/compilation/output/linking/flag_static.sh
+++ test/cases/compilation/output/linking/flag_static.sh
@@ -2,22 +2,26 @@
 
 # REQUIRES: shell
 
+# RUN: echo "int foo();" > %T/libflag_static.h
 # RUN: echo "int foo() { return 1; }" > %T/libflag_static.c
 # RUN: gcc -c -o %T/libflag_static.o %T/libflag_static.c
 # RUN: ar -q -c %T/libflag_static.a %T/libflag_static.o
+
 # RUN: touch %T/libflag_static.%{dynamic_lib_extension}
 
 # RUN: mkdir -p %T/other
-# RUN: echo "int foo1() { return 1; }" > %T/other/libflag_static.c
+
+# RUN: echo "int foo1();" > %T/other/libflag_static.h
+# RUN: echo "int foo1() { return 2; }" > %T/other/libflag_static.c
 # RUN: gcc -c -o %T/other/libflag_static.o %T/other/libflag_static.c
 # RUN: ar -q -c %T/other/libflag_static.a %T/other/libflag_static.o
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
 # RUN: assert_compilation %t.json count -eq 1
-# RUN: assert_compilation %t.json contains -file %T/flag_static.c -files %T/other/libflag_static.a -directory %T -arguments %{c_compiler} -c -L ./other/ -L. -lflag_static -static -o flag_static.c.o flag_static.c
+# RUN: assert_compilation %t.json contains -file %T/flag_static.c -files %T/other/libflag_static.a -directory %T -arguments %{c_compiler} -c -L ./other/ -L. -static -lflag_static -o flag_static.c.o flag_static.c
 # RUN: assert_compilation %t_link.json count -eq 1
-# RUN: assert_compilation %t_link.json contains -files %T/other/libflag_static.a %T/flag_static.c.o -directory %T -arguments %{c_compiler} -L ./other/ -L. -lflag_static -static flag_static.c.o -o flag_static
+# RUN: assert_compilation %t_link.json contains -files %T/flag_static.c.o %T/other/libflag_static.a -directory %T -arguments %{c_compiler} -L ./other/ -L. -static flag_static.c.o -lflag_static -o flag_static
 
-echo "int main() { return 0; }" > flag_static.c
+echo "#include "libflag_static.h" int main() { return foo(); }" > flag_static.c
 
-$CC -o flag_static -L ./other/ -L. -lflag_static -static flag_static.c
+$CC -o flag_static -L ./other/ -L. -static flag_static.c -lflag_static
