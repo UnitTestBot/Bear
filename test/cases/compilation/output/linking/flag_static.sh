b28670ccf76d8a97ea9aeaa64eb0aa5b98d96579
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-16 15:25:00 +0300

    Fix tests for mac-os

--- test/cases/compilation/output/linking/flag_static.sh
+++ test/cases/compilation/output/linking/flag_static.sh
@@ -3,13 +3,13 @@
 # REQUIRES: shell
 
 # RUN: echo "int foo() { return 1; }" > %T/libflag_static.c
-# RUN: gcc -o %T/libflag_static.o %T/libflag_static.c
+# RUN: gcc -c -o %T/libflag_static.o %T/libflag_static.c
 # RUN: ar -q -c %T/libflag_static.a %T/libflag_static.o
 # RUN: touch %T/libflag_static.%{dynamic_lib_extension}
 
 # RUN: mkdir -p %T/other
 # RUN: echo "int foo1() { return 1; }" > %T/other/libflag_static.c
-# RUN: gcc -o %T/other/libflag_static.o %T/other/libflag_static.c
+# RUN: gcc -c -o %T/other/libflag_static.o %T/other/libflag_static.c
 # RUN: ar -q -c %T/other/libflag_static.a %T/other/libflag_static.o
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
