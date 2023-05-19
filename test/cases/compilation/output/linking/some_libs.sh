c53a4ff96428294bedcc293c1de4e082c728434f
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-19 14:31:38 +0300

    Fix tests

--- test/cases/compilation/output/linking/some_libs.sh
+++ test/cases/compilation/output/linking/some_libs.sh
@@ -2,18 +2,33 @@
 
 # REQUIRES: shell
 
-# RUN: touch %T/libsome_libs.a
-# RUN: touch %T/libsome_libs.%{dynamic_lib_extension}
+# RUN: printf "int foo() { return 1; }" > %T/libsome_libs.c
+# RUN: gcc -c %T/libsome_libs.c -o %T/libsome_libs.o
+# RUN: ar -q -c %T/libsome_libs.a %T/libsome_libs.o
 
 # RUN: mkdir -p %T/other
-# RUN: touch %T/other/libsome_libs.a
+# RUN: printf "int foo() { return 1; }" > %T/other/libsome_libs.c
+# RUN: gcc -c %T/other/libsome_libs.c -o %T/other/libsome_libs.o
+# RUN: ar -q -c %T/other/libsome_libs.a %T/other/libsome_libs.o
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
 # RUN: assert_compilation %t.json count -eq 1
-# RUN: assert_compilation %t.json contains -file %T/some_libs.c -files %T/libsome_libs.%{dynamic_lib_extension} -directory %T -arguments %{c_compiler} -c -L. -L ./other/ -lsome_libs -o some_libs.c.o some_libs.c
+# RUN: assert_compilation %t.json contains -file %T/some_libs.c -files %T/libsome_libs.%{dynamic_lib_extension} -directory %T -arguments %{c_compiler} -c -L ./other -L. -lsome_libs -o some_libs.c.o some_libs.c
 # RUN: assert_compilation %t_link.json count -eq 1
-# RUN: assert_compilation %t_link.json contains -files %T/libsome_libs.%{dynamic_lib_extension} %T/some_libs.c.o -directory %T -arguments %{c_compiler} -L. -L ./other/ -lsome_libs some_libs.c.o -o some_libs
+# RUN: assert_compilation %t_link.json contains -files %T/libsome_libs.%{dynamic_lib_extension} %T/some_libs.c.o -directory %T -arguments %{c_compiler} -L ./other -L. -lsome_libs some_libs.c.o -o some_libs
+
+PREFIX="some_libs";
+if [ $(uname | grep -i "darwin") ]; then
+  LD_FLAGS="-o lib${PREFIX}.dylib -dynamiclib -install_name @rpath/${PREFIX}"
+else
+  LD_FLAGS="-o lib${PREFIX}.so -shared -Wl,-soname,${PREFIX}"
+fi
+
+echo "int bar() { return 2; }" > libsome_libs.c;
+$CC -c -o libsome_libs.o -fpic libsome_libs.c;
+$CC ${LD_FLAGS} some_libs.o;
+
 
 echo "int main() { return 0; }" > some_libs.c
 
-$CC -o some_libs -L. -L ./other/ -lsome_libs some_libs.c
+$CC -o some_libs -L ./other -L. -lsome_libs some_libs.c
