807210cb1215db4e9b91dbbde5cbc7b9fc0136e0
Maria Kozlovtseva <mashak1407@gmail.com>
2023-05-19 14:08:17 +0300

    Fix tests

--- test/cases/compilation/output/linking/with_static_lib.sh
+++ test/cases/compilation/output/linking/with_static_lib.sh
@@ -2,7 +2,9 @@
 
 # REQUIRES: shell
 
-# RUN: touch %T/libwith_static_lib.a
+# RUN: printf "int foo() { return 1; }" > %T/libwith_static_lib.c
+# RUN: gcc -c %T/libwith_static_lib.c -o %T/libwith_static_lib.o
+# RUN: ar -q -c %T/libwith_static_lib.a %T/libwith_static_lib.o
 
 # RUN: cd %T; %{bear} --verbose --with-link --output-compile %t.json --output-link %t_link.json -- %{shell} %s
 # RUN: assert_compilation %t.json count -eq 1
